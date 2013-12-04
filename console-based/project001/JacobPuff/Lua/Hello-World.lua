744bc1c959884ccf9eae265723a7bf1b860d8255
JacobPuff <jacob@todandlorna.com>
2013-12-03 18:59:06 -0700

    Added project one for JacobPuff!

---
+++ console-based/project001/JacobPuff/Lua/Hello-World.lua
@@ -0,0 +1,14 @@
+local clock = os.clock
+function sleep(n)  -- seconds
+  local t0 = clock()
+  while clock() - t0 <= n do end
+end
+-- warning: clock can eventually wrap around for sufficiently large n
+-- (whose value is platform dependent).  Even for n == 1, clock() - t0
+-- might become negative on the second that clock wraps.
+
+
+print "Hello World."
+sleep (7)
+print "goodbye."
+sleep (3)
