e6c15981d48425aa9ea45b71789b6e1ce911c966
Tod Hansmann <todpunk@gmail.com>
2014-02-16 15:30:31 -0700

    Experimenting with Java workflows

---
+++ console-based/project001/TodPunk/Java/Main.java
@@ -0,0 +1,11 @@
+/*
+	The java set for TodPunk is a foray into Java within Eclipse,
+	and thus it may have some additional pieces beyond just the java
+	source that may be helpful to that environment.
+*/
+
+public class Main {
+	public static void main(string[] args){
+		System.out.println("Hello, World");
+	}
+}
